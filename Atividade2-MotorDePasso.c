e303c905841f4e2ee3e3eb0ba635dbea8a87972f
Jujupedro5052005 <candido.joao139@gmail.com>
2026-04-14 20:25:24 -0300

    Funcionando a base da atividade

--- Atividade2-MotorDePasso.c
+++ Atividade2-MotorDePasso.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/adc.h"
+
+// TODO: filtro ADC (tirar ruidos)
+// TODO: ADC com interrupcao
 
 #define IN1 2
 #define IN2 3
 #define IN3 4
 #define IN4 5
+
+// Sequencia de half steps
+int mat_seq[8][4] = {
+    {1, 0, 0, 0},
+    {1, 1, 0, 0},
+    {0, 1, 0, 0},
+    {0, 1, 1, 0},
+    {0, 0, 1, 0},
+    {0, 0, 1, 1},
+    {0, 0, 0, 1},
+    {1, 0, 0, 1},
+};
+
+// Incializa variaveis
+int passo_atual = 0;
+int passo_setpoint = 0;
 
 
 // Funcao chamada pelo repeating timer
@@ -16,74 +36,35 @@ bool step_callback(struct repeating_timer *t){
     // esse igual a false so funciona na criacao
     // em outras chamadas, ele apenas pega o valor anterior que ja estava
     static int step_state = 1;
-
-    //printf("%i\n", step_state);
+    static int passos = 0;
 
     // Implementacao da sequencia em half step
-    switch (step_state){
-        case 1:
-            printf("caso 1\n");
-            gpio_put(IN1, true);
-            gpio_put(IN2, false);
-            gpio_put(IN3, false);
-            gpio_put(IN4, false);
-            break;
-        case 2:
-            printf("caso 2\n");
-            gpio_put(IN1, true);
-            gpio_put(IN2, true);
-            gpio_put(IN3, false);
-            gpio_put(IN4, false);
-            break;
-        case 3:
-            printf("caso 3\n");
-            gpio_put(IN1, false);
-            gpio_put(IN2, true);
-            gpio_put(IN3, false);
-            gpio_put(IN4, false);
-            break;
-        case 4:
-            printf("caso 4\n");
-            gpio_put(IN1, false);
-            gpio_put(IN2, true);
-            gpio_put(IN3, true);
-            gpio_put(IN4, false);
-            break;
-        case 5:
-            printf("caso 5\n");
-            gpio_put(IN1, false);
-            gpio_put(IN2, false);
-            gpio_put(IN3, true);
-            gpio_put(IN4, false);
-            break;
-        case 6:
-            printf("caso 6\n");
-            gpio_put(IN1, false);
-            gpio_put(IN2, false);
-            gpio_put(IN3, true);
-            gpio_put(IN4, true);
-            break;
-        case 7:
-            printf("caso 7\n");
-            gpio_put(IN1, false);
-            gpio_put(IN2, false);
-            gpio_put(IN3, false);
-            gpio_put(IN4, true);
-            break;
-        case 8:
-            printf("caso 8\n");
-            gpio_put(IN1, true);
-            gpio_put(IN2, false);
-            gpio_put(IN3, false);
-            gpio_put(IN4, true);
-            break;
+    gpio_put(IN1, mat_seq[step_state][0]);
+    gpio_put(IN2, mat_seq[step_state][1]);
+    gpio_put(IN3, mat_seq[step_state][2]);
+    gpio_put(IN4, mat_seq[step_state][3]);
+
+    passos = passo_setpoint - passo_atual;
+    // Incrementar
+    if (passos > 0){
+        if (step_state >= 8){
+            step_state = 1;
+        } 
+        else {
+            step_state++;
+            passo_atual++;
+        }
     }
 
-    if (step_state >= 8){
-        step_state = 1;
-    } 
-    else {
-        step_state = step_state + 1;
+    // Decrementar
+    if (passos < 0){
+        if (step_state <= 1){
+            step_state = 8;
+        } 
+        else {
+            step_state--;
+            passo_atual--;
+        }
     }
 
     return true;
@@ -117,13 +98,26 @@ int main()
     // Identificador (usamos NULL) e o endereco do timer
     add_repeating_timer_ms(5, step_callback, NULL, &timer);
 
+    bool first_time = true;
     while (true) {
         uint16_t raw = adc_read();
-        float v = raw * 3.3f /4095;
-        // 3.3f é o fundo de escala analogico do ADC da pico (ate quanto a porta le)
+        // analise dimensional:
+        // 3.3f eh o fundo de escala analogico do ADC da pico (ate quanto a porta le)
         // 4095 eh o fundo de escala digital do ADC (12 bits)
-        printf("adc: %u, %.3f\n", raw, v);
-        sleep_ms(1000);
+        //      float voltage_setpoint = raw * (3.3f / 4095);
+        // 300° eh o angulo de abertura maximo de um tipico potenciometro B10K
+        //      float angulo_setpoint = voltage_setpoint * (300 / 3.3f);
+        // motor de passo tem 64 passos correspondendo a 5,625°
+        //      float passo_setpoint = angulo_setpoint * (64 / 5.625);
+        // juntando todas estas contas, tem-se:
+        passo_setpoint = truncf((raw * 300.0f * 64.0f) / (4095.0f * 5.625f));
+        printf("raw_adc = %u, pass_setpoint = %i\n", raw, passo_setpoint);
+
+        if (first_time){
+            passo_atual = passo_setpoint;
+            first_time = false;
+        }
+        sleep_ms(1);
     }
     return 0;
 }
