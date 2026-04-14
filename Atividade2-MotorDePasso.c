e2f88a7b20b532e832241c2ddaaace9a506b6524
AbrSerafim <eabreuserafim@gmail.com>
2026-04-14 18:03:04 -0300

    Código agora lê e converte sinal analógico

--- Atividade2-MotorDePasso.c
+++ Atividade2-MotorDePasso.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-// teste
+#include "hardware/adc.h"
 
 int main()
 {
     stdio_init_all();
 
+    // Inicializa o ADC
+    adc_init();
+    adc_gpio_init(26);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // Lê sinal analógico
+        uint16_t raw = adc_read();
+        // Converte sinal analógico em volts
+        float volts = 3.3f/4095.0f*raw; // Tensão lida varia de 0 a 3.3 volts
+
+
     }
 }
